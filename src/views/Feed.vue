0fc102bfcf36e177f2bcff773db0f2bd9a828a3d
Terra Roush <terra.roush@gmail.com>
2021-03-31 12:45:29 -0500

    built out a feed page

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,9 +1,39 @@
 <template>
-  <h1>This is the Feed page</h1>
+  <v-container>
+    <div v-for="meme in memes" :key="meme.id" class="py-5">
+      <meme
+        class="mx-auto"
+        :top="meme.topText"
+        :bottom="meme.bottomText"
+        :imageURL="meme.imageURL"
+      />
+    </div>
+  </v-container>
 </template>
 
 <script>
-export default {};
+import { db } from "../firebase";
+import Meme from "../components/Meme";
+
+export default {
+  components: { Meme },
+  data() {
+    return {
+      memes: [],
+    };
+  },
+  mounted() {
+    db.collection("memes").onSnapshot((snap) => {
+      const memes = snap.docs.map((doc) => {
+        return {
+          ...doc.data(),
+          id: doc.id,
+        };
+      });
+      this.memes = memes;
+    });
+  },
+};
 </script>
 
 <style></style>
