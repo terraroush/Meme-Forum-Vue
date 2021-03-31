0b5f3c443598281697cc6f7dc5c3017aabc6cce1
Terra Roush <terra.roush@gmail.com>
2021-03-31 13:52:56 -0500

    details page with programitic routing

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,9 +1,31 @@
 <template>
-  <h1>This is the Details page</h1>
+  <div v-if="meme">
+    <meme
+      class="mx-auto my-5"
+      :top="meme.topText"
+      :bottom="meme.bottomText"
+      :imageURL="meme.imageURL"
+    />
+  </div>
 </template>
 
 <script>
-export default {};
+import Meme from "../components/Meme.vue";
+import { db } from "../firebase";
+
+export default {
+  components: { Meme },
+  data() {
+    return {
+      meme: null,
+    };
+  },
+  async mounted() {
+    const memeId = this.$route.params.memeId;
+    const snapshot = await db.collection("memes").doc(memeId).get();
+    this.meme = snapshot.data();
+  },
+};
 </script>
 
 <style></style>
