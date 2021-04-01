c3bca80956b70710b003c8e476f46c27fd1cd9e6
Terra Roush <terra.roush@gmail.com>
2021-04-01 14:43:47 -0500

    added userId to meme object

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -51,7 +51,7 @@
 
 <script>
 import Meme from "../components/Meme.vue";
-import { db } from "../firebase";
+import { db, auth } from "../firebase";
 
 export default {
   components: { Meme },
@@ -73,6 +73,7 @@ export default {
         bottomText: this.bottomText,
         imageURL: this.imageURL,
         normalized: `${this.topText.toUpperCase()} ${this.bottomText.toUpperCase()}`,
+        userId: auth.currentUser.uid,
       });
 
       this.$router.push("/feed");
